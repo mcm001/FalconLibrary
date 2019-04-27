b82ba95528b7776c5d9b478381dde739d8fc99af
Matthew Morley <matthew.morley.ca@gmail.com>
2019-04-26 22:10:20 -0700

    Update SwerveDriveSubsystem.kt

--- src/main/kotlin/org/ghrobotics/lib/subsystems/drive/SwerveDriveSubsystem.kt
+++ src/main/kotlin/org/ghrobotics/lib/subsystems/drive/SwerveDriveSubsystem.kt
@@ -1,7 +1,27 @@
 package org.ghrobotics.lib.subsystems.drive
 
 import org.ghrobotics.lib.components.DriveComponent
+import org.ghrobotics.lib.components.EmergencyHandleable
+import org.ghrobotics.lib.components.SwerveDriveComponent
+import org.ghrobotics.lib.components.SwerveModuleComponent
+import org.ghrobotics.lib.localization.SwerveDriveLocalization
+import org.ghrobotics.lib.physics.SwerveDriveKinematics
 
 class SwerveDriveSubsystem(
+        override val modules: List<SwerveModuleComponent>,
+        override val kinematics: SwerveDriveKinematics,
+        override val localization: SwerveDriveLocalization,
+        drivetrainHeightFromGround: Double
+) : SwerveDriveComponent(drivetrainHeightFromGround), EmergencyHandleable {
 
-) : DriveComponent
+    override fun activateEmergency(severity: EmergencyHandleable.Severity) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun recoverFromEmergency() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+
+}
